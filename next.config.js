bf68ec577ac0378f3b47bb8b279ac26f51b90683
littleRabbit0220 <bluecoder0220@gmail.com>
2023-03-02 20:45:21 +0500

    eslint fix

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
     config.plugins.push(new Dotenv());
     return config;
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
