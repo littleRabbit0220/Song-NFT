241b08f78195cd3bc85439bb78347c64d21df69d
littleRabbit0220 <bluecoder0220@gmail.com>
2023-03-02 21:01:04 +0500

    table margin

--- src/components/leadboard/LeadBoardList.js
+++ src/components/leadboard/LeadBoardList.js
@@ -1,7 +1,7 @@
 const LeadBoardList = () => {
   return (
     <div className='overflow-auto custom-scrollbar pb-2.5'>
-      <div className='min-w-[720px] lead-board-list mt-11 '>
+      <div className='min-w-[720px] lead-board-list mt-11 md:px-4'>
         <div className='flex px-4 pb-1 text-sm md:px-5 text-[#E7E7E8] uppercase'>
           <div className='basis-[20%] md:basis-[22%]'>Username</div>
           <div className='basis-[25%] md:basis-[22%]'>
