079d8afcde2ba95933d2f04b95de1dc0c0ac4fc4
littleRabbit0220 <bluecoder0220@gmail.com>
2023-03-12 20:03:23 +0530

    sample-demo-buywithcrypto

--- src/components/trackPack/TrackDetails.js
+++ src/components/trackPack/TrackDetails.js
@@ -14,8 +14,8 @@ import data from "./data.json"
 const TrackDetails = () => {
   async function handleBuyWithCypto() {
     if (typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       try {
         const TrackPackNFTContract = new ethers.Contract(data.address, data.abi, signer)
